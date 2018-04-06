65ffa3e6895a3778dd542023adfe842eabeec5b6
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-04-06 04:36:07 -0400

    Added 2 tests: first one to test the the default number of counters are being rendered and two: to test that a new counter is being added when the button is clicked

--- counter-list.test.js
+++ counter-list.test.js
@@ -3,11 +3,19 @@ import { shallow, mount } from 'enzyme'
 import CounterList from './counter-list'
 
 describe('CounterList', () => {
-    it('just for demo!', () => {
-        const shallowWrapper = shallow(<CounterList /> )
-        const mountWrapper = mount(<CounterList /> )
+    it('should render two counters by default', () => {
+        const wrapper = shallow(<CounterList /> )
 
-        console.log('shallow HTML', shallowWrapper.debug())
-        console.log('mount HTML', mountWrapper.debug())
+        const counters = wrapper.find('Counter')
+        expect(counters.length).toEqual(2)
+    })
+
+    it('it can add more counter when we click the button', () => {
+        const wrapper = shallow(<CounterList /> )
+
+        const btn = wrapper.find('button')
+        btn.simulate('click')
+        const counters = wrapper.find('Counter')
+        expect(counters.length).toEqual(3)
     })
 })
