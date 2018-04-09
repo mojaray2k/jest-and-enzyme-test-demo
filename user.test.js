776f5df3813914c08c3e8711a3a9be00c865f191
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-04-09 04:50:51 -0400

    refactoring jest spy function

--- user.test.js
+++ user.test.js
@@ -16,23 +16,23 @@ const dummyUser = {
   website: 'https://javascriptplayground.com',
 }
 
+const mockFetchUserResponse = user =>
+  jest
+    .spyOn(api, 'fetchUser')
+    .mockImplementation(() => Promise.resolve(user))
 describe('User', () => {
   //clear out spys between each tests so the tests relies on their own spys
   //This guarantees that one spy does not last outstide of the tests it is set in
   beforeEach(() => jest.restoreAllMocks())
   it('it shows the loading text before the data is fetched', async () => {
-    jest
-        .spyOn(api, 'fetchUser')
-        .mockImplementation(() => Promise.resolve(dummyUser))
+    mockFetchUserResponse(dummyUser)
 
     const wrapper = shallow(<User id={1} />)
     expect(wrapper.find('p').text()).toEqual('Loading!')
   })
 
   it('shows the data once it has been fetched', async () => {
-    jest
-        .spyOn(api, 'fetchUser')
-        .mockImplementation(() => Promise.resolve(dummyUser))
+    mockFetchUserResponse(dummyUser)
 
     const wrapper = shallow(<User id={1} />)
 
