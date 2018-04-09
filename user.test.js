f65f61e0af61ffbdbb0ca74c989f9c7b3a02d930
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-04-09 03:37:22 -0400

    fixed user tests to work for mock http requests

--- user.test.js
+++ user.test.js
@@ -12,11 +12,20 @@ const nextTick = async () => {
 const dummyUser = {
     id: 1,
     name: 'Jack Franklin',
-    website: 'https://javascriptplayground.com'
-}
+    website: 'https://javascriptplayground.com',
+  }
+  
+  const url = 'https://jsonplaceholder.typicode.com/users/1'
+ const mockUrlWithUser = user =>
+    fetchMock.getOnce(url, {
+      status: 200,
+      body: user,
+    })
 
 describe('User', () => {
     it('shows the loading text then the data once it has been fetched', () => {
+        mockUrlWithUser(dummyUser)
+        
         const wrapper = shallow(<User id={1} /> )
         expect(wrapper.find('p').text()).toEqual('Loading!')
     })
