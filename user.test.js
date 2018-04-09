2f2208a3094a8dea0c3c7c209bbbf7fe7d5aca0d
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-04-09 04:47:02 -0400

    adding jest spys to mock data and fixes the duplicate mock requests issue

--- user.test.js
+++ user.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import User from './user'
 import fetchMock from 'fetch-mock'
+import * as api from './api'
 
 const nextTick = async () => {
   return new Promise(resolve => {
@@ -15,21 +16,25 @@ const dummyUser = {
   website: 'https://javascriptplayground.com',
 }
 
-const url = 'https://jsonplaceholder.typicode.com/users/1'
-
-const mockUrlWithUser = user =>
-  fetchMock.getOnce(url, {
-    status: 200,
-    body: user,
-  })
-
 describe('User', () => {
-  
+  //clear out spys between each tests so the tests relies on their own spys
+  //This guarantees that one spy does not last outstide of the tests it is set in
+  beforeEach(() => jest.restoreAllMocks())
   it('it shows the loading text before the data is fetched', async () => {
-    mockUrlWithUser(dummyUser)
+    jest
+        .spyOn(api, 'fetchUser')
+        .mockImplementation(() => Promise.resolve(dummyUser))
 
     const wrapper = shallow(<User id={1} />)
     expect(wrapper.find('p').text()).toEqual('Loading!')
+  })
+
+  it('shows the data once it has been fetched', async () => {
+    jest
+        .spyOn(api, 'fetchUser')
+        .mockImplementation(() => Promise.resolve(dummyUser))
+
+    const wrapper = shallow(<User id={1} />)
 
     await nextTick()
     wrapper.update()
@@ -37,16 +42,4 @@ describe('User', () => {
     expect(wrapper.find('h4').text()).toEqual(dummyUser.name)
     expect(wrapper.find('p').text()).toContain(dummyUser.website)
   })
-
-//   it('shows the data once it has been fetched', async () => {
-//     mockUrlWithUser(dummyUser)
-
-//     const wrapper = shallow(<User id={1} />)
-
-//     await nextTick()
-//     wrapper.update()
-
-//     expect(wrapper.find('h4').text()).toEqual(dummyUser.name)
-//     expect(wrapper.find('p').text()).toContain(dummyUser.website)
-//   })
 })
