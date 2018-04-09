93d763beca0a774b533c1633017eb49a0709291d
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-04-09 04:10:46 -0400

    have a test failing right now working on a fix and added some more dependencies

--- user.test.js
+++ user.test.js
@@ -10,23 +10,43 @@ const nextTick = async () => {
 }
 
 const dummyUser = {
-    id: 1,
-    name: 'Jack Franklin',
-    website: 'https://javascriptplayground.com',
-  }
-  
-  const url = 'https://jsonplaceholder.typicode.com/users/1'
- const mockUrlWithUser = user =>
-    fetchMock.getOnce(url, {
-      status: 200,
-      body: user,
-    })
+  id: 1,
+  name: 'Jack Franklin',
+  website: 'https://javascriptplayground.com',
+}
+
+const url = 'https://jsonplaceholder.typicode.com/users/1'
+
+const mockUrlWithUser = user =>
+  fetchMock.getOnce(url, {
+    status: 200,
+    body: user,
+  })
 
 describe('User', () => {
-    it('shows the loading text then the data once it has been fetched', () => {
-        mockUrlWithUser(dummyUser)
-        
-        const wrapper = shallow(<User id={1} /> )
-        expect(wrapper.find('p').text()).toEqual('Loading!')
-    })
+  
+  it('it shows the loading text before the data is fetched', async () => {
+    mockUrlWithUser(dummyUser)
+
+    const wrapper = shallow(<User id={1} />)
+    expect(wrapper.find('p').text()).toEqual('Loading!')
+
+    await nextTick()
+    wrapper.update()
+
+    expect(wrapper.find('h4').text()).toEqual(dummyUser.name)
+    expect(wrapper.find('p').text()).toContain(dummyUser.website)
+  })
+
+//   it('shows the data once it has been fetched', async () => {
+//     mockUrlWithUser(dummyUser)
+
+//     const wrapper = shallow(<User id={1} />)
+
+//     await nextTick()
+//     wrapper.update()
+
+//     expect(wrapper.find('h4').text()).toEqual(dummyUser.name)
+//     expect(wrapper.find('p').text()).toContain(dummyUser.website)
+//   })
 })
